ba1b5ffb78e052b7f1db63628c9cf2ff1dc281b8
Sauroncool <ambujnayan919@gmail.com>
2023-11-21 01:23:56 +0530

    Edited

--- Assignment 2/BFGS.jl
+++ Assignment 2/BFGS.jl
@@ -1,6 +1,6 @@
-#func(x) = (x[1]^2 + 2 * x[2]^2 - 0.3 * cos(3 * pi * x[1]) - 0.4 * cos(4 * pi * x[2]) + 0.7) #Bohachevsky Function
+func(x) = (x[1]^2 + 2 * x[2]^2 - 0.3 * cos(3 * pi * x[1]) - 0.4 * cos(4 * pi * x[2]) + 0.7) #Bohachevsky Function
 #func(x) = x[1] - x[2] + 2*x[1]^2 + 2*x[1]*x[2] + x[2]^2
-func(x) = (x[1] + 2 * x[2] - 7)^2 + (2 * x[1] + x[2] - 5)^2
+#func(x) = (x[1] + 2 * x[2] - 7)^2 + (2 * x[1] + x[2] - 5)^2
 
 function grad(f, x)
     x = Float64.(x)
@@ -20,16 +20,16 @@ end
 
 grad_f(x) = grad(func, x)
 
-X1 = [1, 2]
-#X1 = [0, 0]
+X1 = [2, 1]
+#X1 = [1, 2]
 del_f1 = grad_f(X1)
 B1 = [1 0
     0 1]
 
 
-function backtracking_line_search(f, grad_f, x, p, α=1.0, beta=0.5, c1=1e-4, c2=0.9) # Backtracking Line Search with Wolfe Condition
+function backtracking_line_search(f, grad_f, x, p, α=1.0, β=0.5, c1=1e-4, c2=0.9) # Backtracking Line Search with Wolfe Condition
     while f(x + α .* p) > f(x) + c1 * α * p' * grad_f(x) || p' * grad_f(x + α * p) < c2 * p' * grad_f(x) # Negative of wolfe condition (https://en.wikipedia.org/wiki/Wolfe_conditions)
-        α *= beta # Reducing the length for not satisfying Wolfe Condition
+        α *= β # Reducing the length for not satisfying Wolfe Condition
     end
     return α
 end
@@ -39,8 +39,9 @@ max_iter = 100
 
 for iter in 1:max_iter
     global X1, B1, del_f1
-
-    if sqrt(sum(del_f1 .* del_f1)) <= 10e-6 # Checking Norm
+    if sqrt(sqrt(del_f1'*del_f1)) <= 10e-6 # Checking Norm
+        # println(X1)
+        # println(sqrt(del_f1'*del_f1))
         break
     end
 
@@ -58,8 +59,24 @@ for iter in 1:max_iter
     X1 = X2
     B1 = B2
     del_f1 = del_f2
+    # println(iter)
+    # println(X1)
+    # println(del_f1)
 end
 println("Solution: ", X1)
 println("Function Value: ", func(X1))
 
 println("Solution rouded off: ", round.(X1; digits=3))
+
+# Plots
+# using Plots
+
+# # Generate x and y values for the surface plot
+# x = range(-1, stop=1, length=100)
+# y = range(-1, stop=1, length=100)
+# z = [func([x[i], y[j]]) for i in 1:length(x), j in 1:length(y)]
+
+# # Create 3D surface plot
+# surface_plot = surface(x, y, z, xlabel="x", ylabel="y", zlabel="f(x, y)", title="Bohachevsky Function",c=:viridis)
+# plotlyjs()
+# plot(surface_plot)
